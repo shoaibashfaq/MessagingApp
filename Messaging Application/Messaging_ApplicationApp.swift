17fc503000bc56e80047611676415679080d7272
Shoaib Ashfaq <shoaibashfaq111@gmail.com>
2024-02-14 12:38:23 -0500

    Initial Commit

---
+++ Messaging Application/Messaging_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Messaging_ApplicationApp.swift
+//  Messaging Application
+//
+//  Created by Shoaib Ashfaq on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Messaging_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
